c7428c9e07881d59b6183dc26bcb21c1c34a0454
Iaroslav Babanin <yvef@yahoo.com>
2024-05-14 13:17:05 +0300

    update bufferline

--- lua/nv-bufferline/init.lua
+++ lua/nv-bufferline/init.lua
@@ -2,14 +2,26 @@ require("bufferline").setup{
 	options = {
 		mode = "buffers",
 		numbers = "ordinal",
+        indicator = {
+            style = "underline"
+        },
 		offsets = {
-			{
+            {
 				filetype = "NvimTree",
 				text = "File Explorer",
 				highlight = "Directory",
 				separator = true
 			}
-		}
-	}
+		},
+        diagnostics = "nvim_lsp",
+        separator_style = "slant"
+	},
+    highlights = {
+        buffer_selected = {
+            fg = "#fdfdfd",
+            bold = true,
+            italic = true,
+        }
+    }
 }
 
